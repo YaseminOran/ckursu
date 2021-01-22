5195392c1f44ef4a75eab0544167bab271a4cef1
yasemin <oran.yasemin@gmail.com>
2021-01-22 23:33:57 +0300

    ADDED:new project

---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Dictionary<int, string> Ogrenci = new Dictionary<int, string>();
+            Ogrenci.Add(134, "Yasemin ARSLAN");
+            Ogrenci.Add(158, "Tayfun ARSLAN");
+            Ogrenci.Add(115, "Bitter");
+            Ogrenci.Add(174, "Persi");
+            Console.Write("Öğrenci No Giriniz:");
+            int No = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine(Ogrenci[No]);
+            }
+            catch
+            {
+                Console.WriteLine("Öğrenci Bulunamadı.");
+            }
+
+
+        }
+    }
+}
